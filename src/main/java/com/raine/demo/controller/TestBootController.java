652a5bd5b9b7d9e18b30d70dfa9885ed0ac53bba
chen_zujun@163 <github1121>
2019-03-27 17:25:49 +0800

    Desc：springboot 读取文件夹测试

--- src/main/java/com/raine/demo/controller/TestBootController.java
+++ src/main/java/com/raine/demo/controller/TestBootController.java
@@ -7,8 +7,20 @@ import com.raine.demo.domain.User;
 import com.raine.demo.service.IUserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.EnableAutoConfiguration;
+import org.springframework.util.ResourceUtils;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.Enumeration;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.jar.JarEntry;
+import java.util.jar.JarFile;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
 
 /**
  * 测试
@@ -21,6 +33,9 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/test/boot")
 public class TestBootController {
 
+    private static final String PATH_STARTS_PREFIX = "BOOT-INF/classes/static/voice/";
+    private final static Pattern PATTERN = Pattern.compile("^BOOT-INF\\/classes\\/static\\/voice\\/(.*\\.mp3)");
+
     @Autowired
     private IUserService userService;
 
@@ -28,5 +43,48 @@ public class TestBootController {
     public User getUser() {
         User user = userService.getUserById(1L);
         return user;
+    }
+
+    @RequestMapping("voice")
+    public List<String> getVoice() throws IOException {
+        List<String> voiceNameList = new ArrayList<>();
+        String path = TestBootController.class.getProtectionDomain().getCodeSource().getLocation().getPath();
+
+        String newPath = path.subSequence(path.indexOf(":") + 1, path.indexOf("!")).toString();
+
+        JarFile localJarFile = new JarFile(new File(newPath));
+        Enumeration<JarEntry> entries = localJarFile.entries();
+        List<String> list = new LinkedList<>();
+        while (entries.hasMoreElements()) {
+            JarEntry jarEntry = entries.nextElement();
+            //获取文件路径
+            String innerPath = jarEntry.getName();
+            if (innerPath.startsWith(PATH_STARTS_PREFIX)) {
+                Matcher matcher = PATTERN.matcher(innerPath);
+                if (matcher.find()) {
+
+                    list.add(matcher.group(1));
+                }
+            }
+        }
+        localJarFile.close();
+
+        voiceNameList.addAll(list);
+        return voiceNameList;
+    }
+
+    @RequestMapping("voice2")
+    public List<String> getVoice2() throws IOException {
+        List<String> voiceNameList = new ArrayList<>();
+        File file = ResourceUtils.getFile("classpath:static/voice");
+        if(file.exists()){
+            File[] files = file.listFiles();
+            if (files != null){
+                for (File child:files){
+                    voiceNameList.add(child.getName());
+                }
+            }
+        }
+        return voiceNameList;
     }
 }
